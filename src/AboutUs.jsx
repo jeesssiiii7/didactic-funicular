12a6063e1c16e9ddcf52a27268c1d55b398a0886
jeesssiiii7 <alfajessica7@gmail.com>
2025-12-18 12:36:05 +0530

    Add AboutUs component with nursery information

---
+++ src/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>Paradise Nursery provides high quality plants and gardening solutions.</p>
+    </div>
+  );
+}
+
+export default AboutUs;

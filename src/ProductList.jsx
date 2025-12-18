7bd8f7f1d0f48beec015126805a771c854e68b89
jeesssiiii7 <alfajessica7@gmail.com>
2025-12-18 12:49:06 +0530

    Create ProductList component to display plants

---
+++ src/ProductList.jsx
@@ -0,0 +1,14 @@
+function ProductList() {
+  return (
+    <div>
+      <h2>Our Plants</h2>
+      <ul>
+        <li>Aloe Vera</li>
+        <li>Snake Plant</li>
+        <li>Peace Lily</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
